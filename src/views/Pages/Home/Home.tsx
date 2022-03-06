87404493b8ed669cc16ce8a1ab4296d255981160
mohammad-aarif <arifbd354@gmail.com>
2022-03-06 12:09:17 +0600

    module issue fixed

--- src/views/Pages/Home/Home.tsx
+++ src/views/Pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Home
+            <div className="text-3xl font-bold underlin">HOme</div>
         </div>
     );
 };
